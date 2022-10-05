6000bdfcdcb8f088c0b6a13385ad8b25395700de
kenken <angelbeatsviolin@gmail.com>
2022-10-05 21:28:09 +0900

    最初に考えた正規表現と、さらに精度が良い正規表現のヒット率を比較する

--- lib/count_ga_tag_from_sql_by_regex.rb
+++ lib/count_ga_tag_from_sql_by_regex.rb
@@ -15,7 +15,7 @@ start_time = Time.now
 # 本番では、SQLファイルを読み込む。
 path = '../plus-staging-dump.20221005.sql'
 
-onclick_counter, ga_tag_counter, honban_ga_tag_counter = 0, 0, 0
+onclick_counter, ga_tag_counter, ga_tag_regex_honban_20221005v1, ga_tag_regex_honban_20221005v2 = 0, 0, 0, 0
 
 File.open(path, 'r').each_line do |read_line|
   next if !read_line.include?('INSERT INTO `nt_posts` VALUES ')
@@ -23,7 +23,6 @@ File.open(path, 'r').each_line do |read_line|
   # 1行の中に、複数のGAタグをある場合、すべてのGAタグを置換する。
   read_line.gsub!(ONCLICK_REGEX) { |matched_text|
     onclick_counter += 1
-    matched_text_by_onclick_regex.puts(matched_text)
     matched_text
   }
 
@@ -32,9 +31,13 @@ File.open(path, 'r').each_line do |read_line|
     matched_text
   }
 
-  read_line.gsub!(GA_TAG_REGEX_HONBAN) { |matched_text|
-    honban_ga_tag_counter += 1
-    matched_text_by_ga_tag_regex_honban.puts(matched_text)
+  read_line.gsub!(GA_TAG_REGEX_HONBAN_20221005v1) { |matched_text|
+    ga_tag_regex_honban_20221005v1 += 1
+    matched_text
+  }
+
+  read_line.gsub!(GA_TAG_REGEX_HONBAN_20221005v2) { |matched_text|
+    ga_tag_regex_honban_20221005v2 += 1
     matched_text
   }
 end
@@ -42,11 +45,13 @@ end
 puts "処理にかかった時間: #{Time.now - start_time}"
 puts "onclick_counter: #{onclick_counter}"
 puts "ga_tag_counter: #{ga_tag_counter}"
-puts "honban_ga_tag_counter: #{honban_ga_tag_counter}"
+puts "GA_TAG_REGEX_HONBAN_20221005v1: #{ga_tag_regex_honban_20221005v1}"
+puts "GA_TAG_REGEX_HONBAN_20221005v2: #{ga_tag_regex_honban_20221005v2}"
 
 
 # OUTPUTS
-# 処理にかかった時間: 18.829566
+# 処理にかかった時間: 36.059347
 # onclick_counter: 71143
 # ga_tag_counter: 714
-# honban_ga_tag_counter: 63741
+# GA_TAG_REGEX_HONBAN_20221005v1: 64046
+# GA_TAG_REGEX_HONBAN_20221005v2: 71118
